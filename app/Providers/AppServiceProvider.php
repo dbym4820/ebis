112c97ab10cd6adfbea9233c315e0e80b375aaab
Tomoki Aburatani <aburatanitomoki@gmail.com>
2018-09-29 20:22:47 +0900

    repair app key

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Shema::defaultStringLength(191);
+        Schema::defaultStringLength(191);
     }
 
     /**
